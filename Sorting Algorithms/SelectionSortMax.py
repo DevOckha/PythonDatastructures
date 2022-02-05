ae0b671ed7d8c28ee6843ff08cc26309c7407d40
AkaOckha <yakopl@hotmail.com>
2022-02-05 17:45:22 +0300

    selection max sort algo..

---
+++ Sorting Algorithms/SelectionSortMax.py
@@ -0,0 +1,18 @@
+list1 = [34, 5, 6, 81, 0, 5]
+print(f"unsorted list {list1}")
+
+for i in range(len(list1) - 1):
+    m_val = list1[i]
+
+    for j in range(i+1, len(list1)):
+        if list1[j] > m_val:
+            m_val = list1[j]
+    
+
+    m_ind = list1.index(m_val, i)
+
+    if list1[i] != list1[m_ind]:
+        list1[i], list1[m_ind] = list1[m_ind], list1[i]
+
+
+print(list1)
