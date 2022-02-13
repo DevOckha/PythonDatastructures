34c6d35c191811f2625c664ba9f242c4fc4d95a6
AkaOckha <yakopl@hotmail.com>
2022-02-13 17:35:40 +0300

    add binary search

---
+++ Sorting Algorithms/binarySearch.py
@@ -0,0 +1,32 @@
+from re import I
+
+
+def binary_search(list1, key):
+    low = 0
+    high = len(list1) - 1
+    Found = False
+    
+    while low <= high and not Found:
+        mid = (low + high) // 2
+        
+        if key == list1[mid]:
+            Found = True
+        elif key > list1[mid]:
+            low = mid + 1
+        
+        else:
+            high = mid - 1
+    
+    if Found == True:
+        print('key is found')
+    else:
+        print("key is not found")
+
+
+
+list1 = [23, 1, 4, 2, 3]
+list1.sort()
+print(list1)
+
+key = int(input('enter the key: '))
+binary_search(list1, key)
