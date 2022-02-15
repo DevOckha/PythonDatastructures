0ec6a04cc1e036acbe6a10b91ba80807dd5358c9
AkaOckha <yakopl@hotmail.com>
2022-02-15 17:22:26 +0300

    add queue

---
+++ example/queue.py
@@ -0,0 +1,30 @@
+class Queue(object):
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return self.items == []
+    
+    
+    def enqueue(self, item):
+        return self.items.insert(0, item)
+    
+    
+    def dequeue(self):
+        return self.items.pop()
+    
+    
+    def size(self):
+        return len(self.items)
+
+
+queue = Queue()
+
+queue.enqueue('Red')
+queue.enqueue('Green')
+queue.enqueue('Blue')
+queue.enqueue('Yellow')
+
+print(queue.items)
+
+print(queue.dequeue())
