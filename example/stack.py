5c02c63f9db375b6cd73ce8690e9d1289f697ae9
AkaOckha <yakopl@hotmail.com>
2022-02-15 17:15:32 +0300

    add stack

---
+++ example/stack.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    
+    def isEmpty(self):
+        return self.items == []
+    
+    
+    def push(self, item):
+        return self.items.append(item)
+    
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[len(self.items) - 1]
+    
+    def size(self):
+        return len(self.items)
+    
+
+
+stack = Stack()
+
+stack.push('Red')
+stack.push('Green')
+stack.push('Blue')
+stack.push('Yellow')
+
+print(stack.items)
+
+stack.pop()
+
+print(stack.items)
