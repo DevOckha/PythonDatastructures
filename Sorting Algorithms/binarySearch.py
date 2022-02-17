ebae5f8b127ebc5aaee7e7c6fe864ce6acd06730
AkaOckha <yakopl@hotmail.com>
2022-02-17 18:28:48 +0300

    insertion and binary

--- Sorting Algorithms/binarySearch.py
+++ Sorting Algorithms/binarySearch.py
@@ -30,3 +30,24 @@ print(list1)
 
 key = int(input('enter the key: '))
 binary_search(list1, key)
+
+
+def binarySearch(list1, key):
+    low = 0
+    high = len(list1) - 1
+    found = False
+    
+    while low <= high and not found:
+        mid = (low + high) // 2
+        
+        if key == list1[mid]:
+            found = True
+        elif key > list1[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    
+    if found == True:
+        print("key is found")
+    else:
+        print("key is not found")
